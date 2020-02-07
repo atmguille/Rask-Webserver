91d8dcadca87651eb1e862511e1da237940fb95e
Guillermo Garcia <ggcobo806@gmail.com>
2020-02-07 21:52:47 +0100

    Implement functions dealing with connections

--- srclib/sockets.c
+++ srclib/sockets.c
@@ -1,10 +1,15 @@
 #include "lib/sockets.h"
 
-int open_tcp_socket(int port, char* ip, int max_pending_conections) {
+int open_tcp_socket(int port, int max_pending_conections) {
     int sockfd;
     struct sockaddr_in myaddr;
 
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+    if (port < 0 || max_pending_conections < 0) {
+        perror("Arguments must be positive");
+        return EXIT_FAILURE;
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Error when creating the socket");
         return EXIT_FAILURE;
     }
@@ -12,19 +17,70 @@ int open_tcp_socket(int port, char* ip, int max_pending_conections) {
     bzero(&myaddr, sizeof(myaddr));
     myaddr.sin_family = AF_INET;
     myaddr.sin_port = htons(port);
-    inet_aton(ip, &myaddr.sin_addr);
+    myaddr.sin_addr.s_addr = INADDR_ANY;
 
-    if ( bind(sockfd, (struct sockaddr*)&myaddr, sizeof(myaddr)) != 0 ) {
+    if (bind(sockfd, (struct sockaddr*)&myaddr, sizeof(myaddr)) != 0) {
         perror("socket--bind");
         return EXIT_FAILURE;
     }
 
-    if ( listen(sockfd, max_pending_conections) != 0 ) {
+    if (listen(sockfd, max_pending_conections) != 0) {
         perror("socket--listen");
         return EXIT_FAILURE;
     }
 
     return sockfd;
+}
+
+int accept_connection(int sockfd) {
+    int clientfd;
+
+    if (sockfd < 0) {
+        perror("Bad socket descriptor when accepting conexion");
+        return EXIT_FAILURE;
+    }
+
+    if ((clientfd = accept(sockfd, NULL, NULL)) < 0) { // Information from the client is not needed
+        perror("Error accepting connection");
+        return EXIT_FAILURE;
+    }
+
+    return clientfd;
+}
+
+ssize_t my_send(int clientfd, const void *buf, size_t len) {
+    ssize_t bytes_sent;
+
+    if (buf == NULL) {
+        perror("Buffer is NULL when sending message");
+        return EXIT_FAILURE;
+    }
+
+    if ((bytes_sent = send(clientfd, buf, len, 0)) < 0) { // We are not going to use FLAGS
+        perror("No bytes sent");
+        return EXIT_FAILURE;
+    }
+
+    syslog(LOG_DEBUG, "%d bytes sent", bytes_sent);
+
+    return bytes_sent;
+}
+
+ssize_t my_recv(int clientfd, void *buf, size_t len) {
+    ssize_t bytes_received;
+
+    if ((bytes_received = recv(clientfd, buf, len, 0)) < 0) { // We are not going to use FLAGS
+        perror("No bytes received");
+        return EXIT_FAILURE;
+    }
+
+    syslog(LOG_DEBUG, "%d bytes received", bytes_received);
+
+    return bytes_received;
+}
+
+void close_connection(int clientfd) {
+    close(clientfd)
 }
 
 void close_socket(int sockfd) {
