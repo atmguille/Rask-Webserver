ba68c3619cbd06deff6bdc528a5620c04de98e86
Daniel Gallo Fernández <daniel.gallo@estudiante.uam.es>
2020-02-08 21:58:41 +0100

    Enable parametrized logging

--- lib/logging/logging.h
+++ lib/logging/logging.h
@@ -3,33 +3,33 @@
 
 /**
  * Prints a critical message to stderr or to the system log if the process is a daemon
- * @param string to be printed
+ * @param format of the message that will be printed
  */
-void print_critical(const char* string);
+void print_critical(const char* string, ...);
 
 /**
  * Prints an error message to stderr or to the system log if the process is a daemon
- * @param string to be printed
+ * @param format of the message that will be printed
  */
-void print_error(const char* string);
+void print_error(const char* format, ...);
 
 /**
  * Prints a warning message to stdout or to the system log if the process is a daemon
- * @param string to be printed
+ * @param format of the message that will be printed
  */
-void print_warning(const char* string);
+void print_warning(const char* format, ...);
 
 /**
  * Prints an info message to stdout or to the system log if the process is a daemon
- * @param string to be printed
+ * @param format of the message that will be printed
  */
-void print_info(const char* string);
+void print_info(const char* format, ...);
 
 /**
  * Prints a debug message to stdout or to the system log if the process is a daemon
- * @param string to be printed
+ * @param format of the message that will be printed
  */
-void print_debug(const char* string);
+void print_debug(const char* format, ...);
 
 /**
  * Sets the logging limit, so the print_ functions will only work if the priority is higher than the threshold set here
