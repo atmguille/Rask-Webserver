4c3bcbc0859d494336a2a89a64efa17bda94f73a
Guillermo Garcia <ggcobo806@gmail.com>
2020-02-12 23:43:12 +0100

    Start Main server structure

--- src/server.c
+++ src/server.c
@@ -1,11 +1,29 @@
 #include "../srclib/socket/socket.h"
 #include "../includes/thread_pool.h"
+#include "../includes/config_parser.h"
 #include <unistd.h>
 
 int main() {
-    int port = 8080;
-    int sockfd = socket_open(port, 10);
-    ThreadPool* t_pool = thread_pool_ini(sockfd, 8);
-    sleep(1000);
-    thread_pool_destroy(t_pool);
+    ServerAttributes    *serverAttributes;
+    int                  socked_fd;
+    ThreadPool          *threadPool;
+
+    serverAttributes = server_attr_load("../files/server.conf");
+    if (serverAttributes == NULL) {
+        return 1;
+    }
+    socked_fd = socket_open(serverAttributes->listen_port, 10);
+    if (socked_fd < 0) {
+        server_attr_destroy(serverAttributes);
+        return 1;
+    }
+
+    threadPool = thread_pool_ini(socked_fd, 8);
+    if (threadPool == NULL) {
+        server_attr_destroy(serverAttributes);
+        return 1;
+    }
+    sleep(1);
+    thread_pool_destroy(threadPool);
+    server_attr_destroy(serverAttributes);
 }
