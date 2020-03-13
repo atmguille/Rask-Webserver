3f46fe0e5f7834cc7679a4e58a7054442209147e
Daniel Gallo Fernández <daniel.gallo@estudiante.uam.es>
2020-03-13 12:14:44 +0100

    Make client use HTTP

--- tests/client.py
+++ tests/client.py
@@ -1,33 +1,35 @@
-import socket
+import string
+import random
+import urllib.request
 from concurrent.futures import ThreadPoolExecutor
 from timeit import default_timer as timer
 
-SERVER_ADDRESS = "127.0.0.1"
+SERVER_ADDRESS = "localhost"
 SERVER_PORT = 8080
-MESSAGE = bytes("a" * 1024, "utf8")
+RESOURCE = "teruel.mp4"
+EXTENSION = RESOURCE[RESOURCE.find('.'):]
 
 
-def send_data(num_messages):
-    for _ in range(num_messages):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((SERVER_ADDRESS, SERVER_PORT))
-            sock.send(MESSAGE)
-            response = sock.recv(4096)
-            sock.close()
+def get_data(messages):
+    for _ in range(messages):
+        filename = ''.join(random.choice(string.ascii_letters) for _ in range(30))
+        urllib.request.urlretrieve(f"http://{SERVER_ADDRESS}:{SERVER_PORT}/{RESOURCE}",
+                                   f"received/{filename}.{EXTENSION}")
 
 
-def start_stress_test(num_threads=8, num_messages=100):
+def start_stress_test(num_threads=8, messages_per_thread=1):
     start = timer()
 
     with ThreadPoolExecutor(max_workers=num_threads) as pool:
         for _ in range(num_threads):
-            pool.submit(send_data, num_messages//num_threads)
+            pool.submit(get_data, messages_per_thread)
 
     end = timer()
     elapsed = end - start
+    num_messages = num_threads * messages_per_thread
     speed = num_messages / elapsed
 
-    print(f"[+] Took {elapsed} s to parse {num_messages} messages: average speed of {speed} messages/s")
+    print(f"[+] Took {elapsed} s to parse {num_threads} messages: average speed of {speed} messages/s")
 
 
-start_stress_test(num_threads=10, num_messages=100000)
+start_stress_test(num_threads=8, messages_per_thread=10)
